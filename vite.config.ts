70cef976d5efe5d16ff523fb2b19c99b56cb7199
vandakisaeed <vandakisaeed@gmail.com>
2025-09-22 13:32:38 +0200

    add ci.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,28 @@
+// /// <reference types="vitest/config" />
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tailwindcss from '@tailwindcss/vite';
+// import { dirname, resolve } from 'node:path';
+// import { fileURLToPath } from 'node:url';
+ 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+ 
+// // https://vite.dev/config/
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, './src')
+//     }
+//   },
+//   plugins: [react(), tailwindcss()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: ['./src/vitest.setup.ts']
+//   }
+// });
+
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -19,6 +44,15 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/vitest.setup.ts']
+    setupFiles: ['./src/vitest.setup.ts'],
+    coverage: {
+      provider: 'istanbul', // Istanbul JS is a test coverage tool
+      reporter: ['text', 'json', 'html'],
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        statements: 80
+      }
+    }
   }
 });
